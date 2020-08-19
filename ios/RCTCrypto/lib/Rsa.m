10b466158c905ff185eb4eef06354dac28a7fa15
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2020-08-19 16:27:54 -0300

    [FIX] Use RSA-OAEP256 Padding

--- ios/RCTCrypto/lib/Rsa.m
+++ ios/RCTCrypto/lib/Rsa.m
@@ -99,11 +99,11 @@ typedef void (^SecKeyPerformBlock)(SecKeyRef key);
     void(^encryptor)(SecKeyRef) = ^(SecKeyRef publicKey) {
         BOOL canEncrypt = SecKeyIsAlgorithmSupported(publicKey,
                                                      kSecKeyOperationTypeEncrypt,
-                                                     kSecKeyAlgorithmRSAEncryptionPKCS1);
+                                                     kSecKeyAlgorithmRSAEncryptionOAEPSHA256);
         if (canEncrypt) {
             CFErrorRef error = NULL;
             cipherText = (NSData *)CFBridgingRelease(SecKeyCreateEncryptedData(publicKey,
-                                                                               kSecKeyAlgorithmRSAEncryptionPKCS1,
+                                                                               kSecKeyAlgorithmRSAEncryptionOAEPSHA256,
                                                                                (__bridge CFDataRef)data,
                                                                                &error));
             if (!cipherText) {
@@ -136,11 +136,11 @@ typedef void (^SecKeyPerformBlock)(SecKeyRef key);
 
         BOOL canDecrypt = SecKeyIsAlgorithmSupported(privateKey,
                                                      kSecKeyOperationTypeDecrypt,
-                                                     kSecKeyAlgorithmRSAEncryptionPKCS1);
+                                                     kSecKeyAlgorithmRSAEncryptionOAEPSHA256);
         if (canDecrypt) {
             CFErrorRef error = NULL;
             clearText = (NSData *)CFBridgingRelease(SecKeyCreateDecryptedData(privateKey,
-                                                                              kSecKeyAlgorithmRSAEncryptionPKCS1,
+                                                                              kSecKeyAlgorithmRSAEncryptionOAEPSHA256,
                                                                               (__bridge CFDataRef)data,
                                                                               &error));
             if (!clearText) {
